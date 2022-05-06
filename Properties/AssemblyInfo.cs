3bd5647991de9ab09b46619d3504810708ad10c2
Chloe Dawn <chloe@sapphic.dev>
2022-05-06 20:33:23 +0100

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("sapphic.dev")]
+[assembly: AssemblyCopyright("Copyright © sapphic.dev 2022")]
+[assembly: AssemblyDescription("A mod for Terraria that suppresses enemy defeat announcements")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("SilentEnemyDefeat")]
+[assembly: AssemblyTitle("SilentenemyDefeat")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ SilentEnemyDefeatPlugin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using JetBrains.Annotations;
+using Terraria;
+using Terraria.Localization;
+using TerrariaApi.Server;
+
+namespace SapphicDev.Terraria.SilentEnemyDefeat;
+
+[ApiVersion(2, 1)]
+[UsedImplicitly]
+public class SilentEnemyDefeatPlugin : TerrariaPlugin
+{
+    public SilentEnemyDefeatPlugin(Main game) : base(game)
+    {
+    }
+
+    public override string Name => "SilentEnemyDefeat";
+
+    public override Version Version => new(1, 0);
+
+    public override string Author => "Chloe Dawn";
+
+    public override string Description => "Suppresses enemy defeat announcements";
+
+    private static void OnServerBroadcast(ServerBroadcastEventArgs e)
+    {
+        if (e.Message is
+            {
+                _text: "Game.EnemiesDefeatedByAnnouncement",
+                _mode: NetworkText.Mode.LocalizationKey
+            })
+        {
+            e.Handled = true;
+        }
+    }
+
+    public override void Initialize()
+    {
+        ServerApi.Hooks.ServerBroadcast.Register(this, OnServerBroadcast);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            ServerApi.Hooks.ServerBroadcast.Deregister(this, OnServerBroadcast);
+        }
+
+        base.Dispose(disposing);
+    }
+}
